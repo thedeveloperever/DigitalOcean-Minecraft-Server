95da4f0ae6f40132f2b390049c3572641aa4b68f
TheDeveloperEver <76601635+thedeveloperever@users.noreply.github.com>
2024-06-24 22:51:43 -0400

    fix bugs

--- init.sh
+++ init.sh
@@ -1,17 +1,13 @@
 ## ----------- Edit Variables here ----------- ##
 
 # Enter modrinth links here one by one e.g. https://cdn.modrinth.com/data/P7dR8mSH/versions/tAwdMmKY/fabric-api-0.97.1%2B1.20.4.jar
-MODS = ""
+MODS=""
 
 # Enter volume name e.g. /mnt/volume_sfo3_01
-VOLUME = ""
+VOLUME=""
 
 # Enter server jar version e.g. https://meta.fabricmc.net/v2/versions/loader/1.20.4/0.15.11/1.0.1/server/jar
-JAR = ""
-
-# Assumes a Debian 12 system with Azul OpenJDK 17, change to your liking
-wget https://cdn.azul.com/zulu/bin/zulu17.50.19-ca-jre17.0.11-linux_amd64.deb -o zre-17.deb
-sudo apt install ./zre-17.deb
+JAR=""
 
 ## ----------- DONT CHANGE ANYTHING BELOW ----------- ##
 cd $VOLUME
